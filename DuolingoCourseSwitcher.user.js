f0b3b7e28d52512d7cee028bde7b0298703b6bc4
zeta12ti <zeta12ti@users.noreply.github.com>
2017-06-30 00:09:05 -0600

    Now simply reorganizes courses

--- DuolingoCourseSwitcher.user.js
+++ DuolingoCourseSwitcher.user.js
@@ -5,9 +5,14 @@
 // @updateURL   https://github.com/zeta12ti/DuolingoCourseSwitcher/raw/master/DuolingoCourseSwitcher.user.js
 // @include     https://*.duolingo.com/*
 // @grant       none
-// @version     0.9.2
-// @author      zeta12ti, arekolek, jrikhal, gmelikov, guillaumebrunerie
+// @version     1.0.0
+// @author      zeta12ti
 // ==/UserScript==
+
+/*
+ * NOTE: Annoyingly, duo.l10n.declared may change with time, which shifts the index for each phrase
+ * If the header on the course menu is wrong, that's probably what happened.
+*/
 
 
 // Language names (in english) - for use with duo.l10n.undeclared
@@ -19,9 +24,9 @@ function getLanguageString(languageCode) {
 
 // language flags - 
 // keys: remove flag- prefix and keep consistency - zs -> zh-CN, kl -> tlh, (zs -> zh-¿?)
-var flags = {"ar": "_1ARRD _3viv6", "bn": "_2TXAL _3viv6", "ca": "mc4rg _3viv6", "cs": "_1uPQW _3viv6", "cy": "_1jO8h _3viv6", "da": "_1h0xh _3viv6", "de": "oboa9 _3viv6", "dk": "_3AA1F _3viv6", "el": "_2tQo9 _3viv6", "en": "_2cR-E _3viv6", "eo": "pWj0w _3viv6", "es": "u5W-o _3viv6", "fr": "_2KQN3 _3viv6", "ga": "_1vhNM _3viv6", "gn": "_24xu4 _3viv6", "he": "_PDrK _3viv6", "hi": "OgUIe _3viv6", "hu": "_1S3hi _3viv6", "id": "_107sn _3viv6", "it": "_1PruQ _3viv6", "ja": "_2N-Uj _3viv6", "ko": "_2lkzc _3viv6", "nl-NL": "_1fajz _3viv6", "no-BO": "_200jU _3viv6", "pl": "_3uusw _3viv6", "pt": "pmGwL _3viv6", "ro": "_12U6e _3viv6", "ru": "_1eqxJ _3viv6", "sn": "q_PD- _3viv6", "sv": "_2DMfV _3viv6", "sw": "_3T1km _3viv6", "th": "_2oTcA _3viv6", "tl": "_1q_MQ _3viv6", "tlh": "_6mRM _3viv6", "tr": "_1tJJ2 _3viv6", "uk": "_1zZsN _3viv6", "un": "t-XH- _3viv6", "vi": "_1KtzC _3viv6", "zh": "xi6jQ _3viv6", "zh-CN": "_2gNgd _3viv6", "_circle-flag": "_2XSZu", "_flag": "_3viv6", "medium-circle-flag": "_1ct7y _2XSZu", "micro-circle-flag": "_3i5IF _2XSZu", "small-circle-flag": "_3PU7E _2XSZu"}
+var flags = {"ar": "_1ARRD", "bn": "_2TXAL", "ca": "mc4rg", "cs": "_1uPQW", "cy": "_1jO8h", "da": "_1h0xh", "de": "oboa9", "dk": "_3AA1F", "el": "_2tQo9", "en": "_2cR-E", "eo": "pWj0w", "es": "u5W-o", "fr": "_2KQN3", "ga": "_1vhNM", "gn": "_24xu4", "he": "_PDrK", "hi": "OgUIe", "hu": "_1S3hi", "id": "_107sn", "it": "_1PruQ", "ja": "_2N-Uj", "ko": "_2lkzc", "nl-NL": "_1fajz", "no-BO": "_200jU", "pl": "_3uusw", "pt": "pmGwL", "ro": "_12U6e", "ru": "_1eqxJ", "sn": "q_PD-", "sv": "_2DMfV", "sw": "_3T1km", "th": "_2oTcA", "tl": "_1q_MQ", "tlh": "_6mRM", "tr": "_1tJJ2", "uk": "_1zZsN", "un": "t-XH-", "vi": "_1KtzC", "zh": "xi6jQ", "zh-CN": "_2gNgd", "_circle-flag": "_2XSZu", "_flag": "_3viv6", "medium-circle-flag": "_1ct7y _2XSZu", "micro-circle-flag": "_3i5IF _2XSZu", "small-circle-flag": "_3PU7E _2XSZu"}
 // Use this to identify a row in the original list.
-var inverse_flags = {"OgUIe _3viv6": "hi", "_107sn _3viv6": "id", "_12U6e _3viv6": "ro", "_1ARRD _3viv6": "ar", "_1KtzC _3viv6": "vi", "_1PruQ _3viv6": "it", "_1S3hi _3viv6": "hu", "_1ct7y _2XSZu": "medium-circle-flag", "_1eqxJ _3viv6": "ru", "_1fajz _3viv6": "nl-NL", "_1h0xh _3viv6": "da", "_1jO8h _3viv6": "cy", "_1q_MQ _3viv6": "tl", "_1tJJ2 _3viv6": "tr", "_1uPQW _3viv6": "cs", "_1vhNM _3viv6": "ga", "_1zZsN _3viv6": "uk", "_200jU _3viv6": "no-BO", "_24xu4 _3viv6": "gn", "_2DMfV _3viv6": "sv", "_2KQN3 _3viv6": "fr", "_2N-Uj _3viv6": "ja", "_2TXAL _3viv6": "bn", "_2XSZu": "_circle-flag", "_2cR-E _3viv6": "en", "_2gNgd _3viv6": "zh-CN", "_2lkzc _3viv6": "ko", "_2oTcA _3viv6": "th", "_2tQo9 _3viv6": "el", "_3AA1F _3viv6": "dk", "_3PU7E _2XSZu": "small-circle-flag", "_3T1km _3viv6": "sw", "_3i5IF _2XSZu": "micro-circle-flag", "_3uusw _3viv6": "pl", "_3viv6": "_flag", "_6mRM _3viv6": "tlh", "_PDrK _3viv6": "he", "mc4rg _3viv6": "ca", "oboa9 _3viv6": "de", "pWj0w _3viv6": "eo", "pmGwL _3viv6": "pt", "q_PD- _3viv6": "sn", "t-XH- _3viv6": "un", "u5W-o _3viv6": "es", "xi6jQ _3viv6": "zh"}
+var inverse_flags = {"OgUIe": "hi", "_107sn": "id", "_12U6e": "ro", "_1ARRD": "ar", "_1KtzC": "vi", "_1PruQ": "it", "_1S3hi": "hu", "_1ct7y _2XSZu": "medium-circle-flag", "_1eqxJ": "ru", "_1fajz": "nl-NL", "_1h0xh": "da", "_1jO8h": "cy", "_1q_MQ": "tl", "_1tJJ2": "tr", "_1uPQW": "cs", "_1vhNM": "ga", "_1zZsN": "uk", "_200jU": "no-BO", "_24xu4": "gn", "_2DMfV": "sv", "_2KQN3": "fr", "_2N-Uj": "ja", "_2TXAL": "bn", "_2XSZu": "_circle-flag", "_2cR-E": "en", "_2gNgd": "zh-CN", "_2lkzc": "ko", "_2oTcA": "th", "_2tQo9": "el", "_3AA1F": "dk", "_3PU7E _2XSZu": "small-circle-flag", "_3T1km": "sw", "_3i5IF _2XSZu": "micro-circle-flag", "_3uusw": "pl", "_3viv6": "_flag", "_6mRM": "tlh", "_PDrK": "he", "mc4rg": "ca", "oboa9": "de", "pWj0w": "eo", "pmGwL": "pt", "q_PD-": "sn", "t-XH-": "un", "u5W-o": "es", "xi6jQ": "zh"}
 
 
 function getLanguageFlag(languageCode) {
@@ -35,131 +40,113 @@ function getLanguageFromFlag(flagCode) {
 }
 
 
-// extract the language switchers that Duolingo uses natively.
-function getNativeLanguageSwitchers() {
-    var flagNodes = document.querySelectorAll('._3vx2Z') // list of small flags ('._2XSZu' would include the larger flag)
-    var switchFunctions = {}
-    var len = flagNodes.length
+async function reorganizeMenu() {
+    var uiLanguage = window.duo.uiLanguage
+    var baseLanguages = {}
+    var courses = document.querySelectorAll('li._2kNgI._1qBnH')
+    var len = courses.length
     for (var i=0; i<len; i++) {
-        var lang = getLanguageFromFlag(flagNodes[i].classList[0] + ' _3viv6')
-        switchFunctions[lang] = flagNodes[i].parentElement.click.bind(flagNodes[i].parentElement)
-    }
+        var menuItem = courses[i]
+        if (menuItem.classList.length > 2) {
+            continue
+        }
 
-    return switchFunctions
-}
+        var flags = menuItem.querySelectorAll('._3viv6')
+        if (typeof menuItem.dataset.from === 'undefined' && typeof menuItem.dataset.learning === 'undefined') {
+            var flags = menuItem.querySelectorAll('._3viv6')
+            if (flags.length === 2) {
+                var learningLanguage = getLanguageFlag(flags[0].classList[0])
+                var fromLanguage = getLanguageFlag(flags[1].classList[0])
+            }
+            else if (flags.length === 1) {
+                var learningLanguage = getLanguageFlag(flags[0].classList[0])
+                var fromLanguage = uiLanguage
+            }
+            else {
+                var learningLanguage = 'un'
+                var fromLanguage = uiLanguage
+            }
+        else {
+            learningLanguage = menuItem.dataset.learning
+            fromLanguage = menuItem.dataset.from
+        }
 
+        if (flags.length === 2) {
+            flags[1].setAttribute('style', 'display: none')
+            flags[0].classList.remove('_2IJLr')
+        }
+        flags[0].classList.add('learning-flag')
+            
 
-// Hides the old menu (keep it around for the native switchers)
-function hideMenuItems() {
-    var menuItems = document.querySelectorAll('._20LC5 > *')
-    var len = menuItems.length
-    for (var i=0; i<len; i++) {
-        menuItems[i].style.display = "none"
-    }
-}
+        menuItem.dataset.learning = learningLanguage
+        menuItem.dataset.from = fromLanguage
+        menuItem.classList.add('learning-course')
 
-// calculates the level given the experience
-function xpToLevel ( xp ) {
-    var xpLevelCutoffs = duoState.config.xpLevelCutoffs
-    var level = 1
-    var len = xpLevelCutoffs.length
-    for (var i = 0; i < len; i++) {
-        if (xp < xpLevelCutoffs[i]) {
-            return level
-        } else {
-            level++
+        if (fromLanguage in baseLanguages) {
+            baseLanguages[fromLanguage].appendChild(menuItem)
+        }
+        else {
+            var fromLanguageItem = addFromLanguageItem(fromLanguage, fromLanguage === uiLanguage)
+            baseLanguages[fromLanguage] = createInnerMenu(fromLanguage, learningLanguage)
+            baseLanguages[fromLanguage].appendChild(menuItem)
+
+            fromLanguageItem.appendChild(baseLanguages[fromLanguage])
+            fromLanguageItem.addEventListener('mouseover', showSubmenuFunction)
+            fromLanguageItem.addEventListener('mouseout', hideSubmenuFunction)
         }
     }
-    return level
-}
 
-// Gets information about all currently learning courses from duo.state
-function getCourseInfo() {
-    var courses = duoState.courses
-    var coursesOrganized = {}
+    
 
-    // I think we have to iterate through like this since courses is a dictionary.
-    for (var course in courses) {
-        if (courses.hasOwnProperty(course)) {
-            var from = courses[course].fromLanguage
-            var learning = courses[course].learningLanguage
-            var xp = courses[course].xp
-            coursesOrganized[from] = coursesOrganized[from]||[] // leave it unchanged if it exists, otherwise initialize (caution: this construction may fail if the value could be falsey, like 0, '' or false)
+function addFromLanguageItem(fromLanguage, currentCourse) {
+    var fromLanguageItem = document.createElement('li')
 
-            coursesOrganized[from].push({'from': from,
-                                         'from_string': getLanguageString(from),
-                                         'learning': learning,
-                                         'learning_string': getLanguageString(learning),
-                                         'xp': xp,
-                                         'level': xpToLevel(xp)
-                                         })
-        }
+    var className = '_2kNgI _1qBnH from-course'
+    if (currentCourse) {
+        className = '_1oVFS ' + className
     }
-    return coursesOrganized
+    fromLanguageItem.setAttribute('class', className)
+    fromLanguageItem.style.cursor = 'default' // clicking on these menus doesn't do anything, so remove the special cursor
+
+    var flag = document.createElement('span')
+    className = getLanguageFlag(fromLanguage) + ' _3viv6 _3vx2Z _1ct7y _2XSZu from-flag'
+    flag.setAttribute('class', className)
+    fromLanguageItem.appendChild(flag)
+
+    var localizedLanguageName = getLanguageString(fromLanguage)
+    fromLanguageItem.appendChild(document.createTextNode(localizedLanguageName))
+
+    return fromLanguageItem
 }
+            
+function  createInnerMenu() {
+    var innerMenu = document.createElement('ul')
+    innerMenu.setAttribute('class', 'OSaWc _1ZY-H language-submenu') // same as top level menu, but without the arrow (_2HujR)
 
+    var innerMenuHeader = document.createElement("li")
+    innerMenuHeader.setAttribute('class', '_2PurW')
+    innerMenuHeader.innerHTML = document.querySelector('_2PurW').innerHTML
+    innerMenu.appendChild(innerMenuHeader)
 
-// Takes an argument of the form given by the output of getCourseInfo
-// Sorts from languages by number of courses and learning languages by xp.
-function sortCourseInfo(courses) {
-    sortedLanguages = Object.keys(courses).sort(function(a, b) { return courses[b].length - courses[a].length })
-    return sortedLanguages.map(function(a) { return courses[a].sort(function(b,c) { return c.xp - b.xp } )})
-}
+    var innerMenuCourseList = document.createElement('ul')
+    innerMenuCourseList.setAttribute('class', '_1XE6M')
+    innerMenu.appendChild(innerMenuCourseList)
 
+    innerMenu.setAttribute('style', 'display: none')
 
-// Both the above in a single step
-function sortedCourseInfo() {
-    return sortCourseInfo(getCourseInfo())
-}
-
-
-// Use duoState to fetch current from and to languages
-function getCurrentLanguages() {
-    return {'from': duoState.user.fromLanguage, 'learning': duoState.user.learningLanguage}
-}
-
-
-// Switch source and target languages - credit to jrikhal
-function switchCourse(SrcLang,TgtLang){
-    //Needed due to inconsistency in Duo's language codes!!!
-    if(SrcLang==='nl-NL'){SrcLang='dn';}
-    if(SrcLang==='no-BO'){SrcLang='nb';}
-    if(SrcLang==='zh-CN'){SrcLang='zs';}
-    if(TgtLang==='nl-NL'){TgtLang='dn';}
-    if(TgtLang==='no-BO'){TgtLang='nb';}
-    if(TgtLang==='zh-CN'){TgtLang='zs';}
-
-    var request=new XMLHttpRequest();
-    request.open("POST", "https://www.duolingo.com/api/1/me/switch_language", true)
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    request.send('learning_language='+TgtLang+'&from_language='+SrcLang)
-
-    waitForSwitchCourseLoad(request)
-}
-
-// Reload page once the course is switched - credit to jrikhal
-function waitForSwitchCourseLoad(request){
-    if(request.readyState<4){
-        setTimeout(function(){ waitForSwitchCourseLoad(request) }, 500)
-    } else location.reload()
-}
-
-
-// Switches source and target languages - using the DOM object as this.
-function switchCourseFunction() {
-    switchCourse(this.getAttribute('data-from'), this.getAttribute('data-to'))
+    return innerMenuCourseList
 }
 
 
 // Show a submenu ('this' is the source language item)
 function showSubmenuFunction() {
-    this.querySelector('.language-submenu').setAttribute('style', 'display: block !important')
+    this.querySelector('.language-submenu').setAttribute('style', 'display: block')
 }
 
 
 // Hide a submenu ('this' is the source language item)
 function hideSubmenuFunction() {
-    this.querySelector('.language-submenu').setAttribute('style', 'display: none !important')
+    this.querySelector('.language-submenu').setAttribute('style', 'display: none')
 }
 
 
@@ -294,86 +281,6 @@ function constructMenu(courses, fromLang, toLang) {
     addNewCourse.appendChild(document.createTextNode(duo.l10n.declared[178]||'Add a new course')) // Ajouter un nouveau cours
     addNewCourse.addEventListener('click', function() {window.location.href = '/courses'})
     topMenu.appendChild(addNewCourse)
-}
-
-
-// Returns a best approximation to the info in duo.state in case the actual isn't available.
-// It won't include all the courses and courses that have the same level but different xp might be sorted wrong,
-// but it should be accurate otherwise.
-function sortaDuoState() {
-    var pseudoDuoState = {}
-    pseudoDuoState.config = {}
-    pseudoDuoState.user = {}
-    pseudoDuoState.config.xpLevelCutoffs = [60, 120, 200, 300, 450, 750, 1125, 1650, 2250, 3000, 3900, 4900, 6000, 7500, 9000, 10500, 12000, 13500, 15000, 17000, 19000, 22500, 26000, 30000]
-    pseudoDuoState.user.fromLanguage = document.querySelector('html').lang
-
-    pseudoDuoState.courses = {}
-    var courseFlags = document.querySelectorAll('._3viv6._3vx2Z._1ct7y._2XSZu')
-    var len = courseFlags.length
-    for (var i=0; i<len; i++) {
-        if (courseFlags[i].classList.contains('new-flag')) { continue } // skip the new flags added by this script
-        var toLang = getLanguageFromFlag(courseFlags[i].classList[0]+' _3viv6')
-        if (courseFlags[i].parentElement.classList.contains('_1oVFS')) {
-            pseudoDuoState.user.learningLanguage = toLang
-        }
-        pseudoDuoState.courses[toLang+'<'+pseudoDuoState.user.fromLanguage] = {'learningLanguage': toLang, 'fromLanguage': pseudoDuoState.user.fromLanguage, 'xp': pseudoDuoState.config.xpLevelCutoffs[courseFlags[i].parentElement.innerText.split(' ').slice(-1)[0] - 2]||0}
-    }
-
-    return pseudoDuoState
-}
-
-
-// Extracts the from/to languages from the html of the page
-function protoLanguages() {
-    var pseudoDuoState = {}
-    pseudoDuoState.fromLanguage = document.querySelector('html').lang
-
-    pseudoDuoState.courses = {}
-    var courseFlags = document.querySelectorAll('._3viv6._3vx2Z._1ct7y._2XSZu')
-    var len = courseFlags.length
-    for (var i=0; i<len; i++) {
-        var toLang = getLanguageFromFlag(courseFlags[i].classList[0]+' _3viv6')
-        if (courseFlags[i].parentElement.classList.contains('_1oVFS')) {
-            pseudoDuoState.learningLanguage = toLang
-        }
-    }
-
-    return pseudoDuoState
-}
-
-
-// Check that duo.state is consistent with what can be derived from the page
-// If not, delay for a bit.
-// Once it's consistent, run the routine.
-function verifyDuoState() {
-    if (document.querySelector('.topbar-right')) {
-        console.log('This is the old site.')
-        return
-    }
-    pseudoDuoState = protoLanguages()||{'fromLanguage': '', 'learningLanguage': ''}
-    actualDuoState = JSON.parse(window.localStorage['duo.state'])
-    if (actualDuoState.user.learningLanguage == pseudoDuoState.learningLanguage && actualDuoState.user.fromLanguage == pseudoDuoState.fromLanguage) {
-        routine()
-    }
-    else {
-        setTimeout(verifyDuoState, 100)
-    }
-}
-
-
-// Prune out those courses that aren't being used anymore.
-function pruneDuoState() {
-    var unprunedDuoState = JSON.parse(window.localStorage['duo.state'])||{}
-    pseudoDuoState = sortaDuoState()
-
-    courseKeys = Object.keys(unprunedDuoState.courses)
-    len = courseKeys.length
-    for (var i=0; i<len; i++) {
-        if (unprunedDuoState.courses[courseKeys[i]].fromLanguage == pseudoDuoState.user.fromLanguage && !pseudoDuoState.courses.hasOwnProperty(courseKeys[i])) {
-             delete unprunedDuoState.courses[courseKeys[i]]
-        }
-    }
-    window.localStorage.setItem('duo.state', JSON.stringify(unprunedDuoState))
 }
 
 
