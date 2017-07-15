9aee24bdc9c31c5b155f0b4a561f019dfedacfa1
zeta12ti <zeta12ti@users.noreply.github.com>
2017-07-14 18:17:56 -0600

    Another new duo version (possibly different version on Chrome?)

--- DuolingoCourseSwitcher.user.js
+++ DuolingoCourseSwitcher.user.js
@@ -5,7 +5,7 @@
 // @updateURL   https://github.com/zeta12ti/DuolingoCourseSwitcher/raw/master/DuolingoCourseSwitcher.user.js
 // @include     https://*.duolingo.com/*
 // @grant       none
-// @version     1.1.5
+// @version     1.1.6
 // @author      zeta12ti
 // ==/UserScript==
 
@@ -16,7 +16,7 @@
 // things to check with new versions:
 //  duo.l10n.declared[146] is Languages
 //  duo.l10n.undeclared[languages[languageCode]] is correct
-console.assert(~['6443752', '0f37dfe'].indexOf(window.duo.version))
+console.assert(~['6443752', '0f37dfe', 'ef39b02'].indexOf(window.duo.version))
 
 // Language names (in english) - for use with duo.l10n.undeclared
 var languages = {'ar': 'Arabic', 'bn': 'Bengali', 'ca': 'Catalan', 'cs': 'Czech', 'cy': 'Welsh', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'eo': 'Esperanto', 'es': 'Spanish', 'fr': 'French', 'ga': 'Irish', 'gn': 'Guarani (Jopar\u00e1)', 'he': 'Hebrew', 'hi': 'Hindi', 'hu': 'Hungarian', 'id': 'Indonesian', 'it': 'Italian', 'ja': 'Japanese', 'ko': 'Korean', 'nl-NL': 'Dutch', 'no-BO': 'Norwegian (Bokm\u00e5l)', 'pa': 'Punjabi (Gurmukhi)', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian', 'ru': 'Russian', 'sv': 'Swedish', 'sw': 'Swahili', 'ta': 'Tamil', 'te': 'Telugu', 'th': 'Thai', 'tl': 'Tagalog', 'tlh': 'Klingon', 'tr': 'Turkish', 'uk': 'Ukrainian', 'vi': 'Vietnamese', 'zh-CN': 'Chinese'}
